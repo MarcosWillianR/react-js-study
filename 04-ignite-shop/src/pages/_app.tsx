890adf8c1a486c1c8a85d166a376d452a67be9e2
Marcos Willian <markusuuuu@gmail.com>
2022-12-22 02:35:42 -0300

    chore: remove unused files and folders, and installed Roboto font with @next/fonts

---
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
+
+export default MyApp
