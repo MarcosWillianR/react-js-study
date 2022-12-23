8c62634019bb87a104fdaadec10089077dd16284
Marcos Willian <markusuuuu@gmail.com>
2022-12-23 00:17:13 -0300

    chore[WIP]: understanding next js Image component

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { Roboto } from '@next/font/google';
 import { AppProps } from 'next/app';
 import { globalStyles } from '../styles/global';
+import Image from 'next/image';
+
 
 import logoSVG from '../assets/logo.svg';
 import { Container, Header } from '../styles/pages/app';
@@ -12,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoSVG} alt="" />
+        <Image src={logoSVG} alt="" />
       </Header>
 
       <main className={roboto.className}>
