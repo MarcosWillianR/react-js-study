ccdbf7cb6fc7f710be6edd93176bf269530a4165
Marcos Willian <markusuuuu@gmail.com>
2022-12-28 21:21:11 -0300

    chore: monorepo config

--- 05-design-system/packages/tokens/src/index.ts
+++ 05-design-system/packages/tokens/src/index.ts
@@ -1,6 +1,8 @@
-export * from './colors';
-export * from './font-sizes';
-export * from './font-weights';
-export * from './line-heights';
-export * from './radii';
-export * from './space';
+export * from './colors'
+
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
+
+export * from './radii'
+export * from './space'
