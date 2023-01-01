b532534feea7855a84ce4c683f49b581fab6e7a6
Marcos Willian <markusuuuu@gmail.com>
2023-01-01 05:35:11 -0300

    feat: created first storybook component

--- 05-design-system/packages/react/src/index.tsx
+++ 05-design-system/packages/react/src/index.tsx
@@ -1,3 +1,30 @@
-export function App() {
-  return <h1>hello world</h1>
-}
+import { ComponentProps } from 'react'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  fontWeight: '$bold',
+  color: '$white',
+  border: 0,
+
+  variants: {
+    variant: {
+      small: {
+        fontSize: '$sm',
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: '$md',
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
