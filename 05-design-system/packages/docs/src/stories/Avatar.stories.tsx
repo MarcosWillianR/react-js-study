c0533a3034def7138bfc120a1c05a01effaae815
Marcos Willian <markusuuuu@gmail.com>
2023-01-06 18:59:46 -0300

    chore: added more storybook controls

--- 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/MarcosWillianR.png',
     alt: 'Marcos Willian',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
