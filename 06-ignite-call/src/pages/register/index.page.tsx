56cb1f57b2b67a727fa769eae199570ab5cafd4b
Marcos Willian <markusuuuu@gmail.com>
2023-01-15 11:54:25 -0300

    feat: creating user and setting up a Cookie

--- 06-ignite-call/src/pages/register/index.page.tsx
+++ 06-ignite-call/src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Container, Form, FormError, Header } from './styles'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '../../lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -37,7 +38,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
